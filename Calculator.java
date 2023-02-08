f541023e54b6efa9a381f3b3085e6310fd2a3fb9
Ceffrece <cheffrece@quinnipiac.edu>
2023-02-08 11:00:23 -0500

    Adds multiplication and division to the calculator

--- Calculator.java
+++ Calculator.java
@@ -12,13 +12,20 @@ public class Calculator {
         int number2 = scan.nextInt(); //Readsin number 2
         int sum = number1 + number2; //Adds together
         int difference = number1 - number2; //Subtracts the two numbers
-        System.out.println("Would you like to: \n 1: Add \n 2: Subtract");
+        int multiplication = number1 * number2; //Multiplies the two numbers
+        int division = number1/number2; //Divides the two numbers
+        System.out.println("Would you like to: \n 1: Add \n 2: Subtract \n 3: Multiply \n 4: Divide");
         System.out.println("Choice> ");
         int choice1 = scan.nextInt();
         switch(choice1) {
             case 1: System.out.println("The sum of " + number1 + " and " + number2 + " is " + sum + ".");
             break;
             case 2: System.out.println("The difference of " + number1 + " and " + number2 + " is " + difference + ".");
+            break;
+            case 3: System.out.println("The product of " + number1 + " and " + number2 + " is " + multiplication + ".");
+            break;
+            case 4: System.out.println("The quotient of " + number1 + " and " + number2 + " is " + division + ".");
+            break;
         }
         
     }

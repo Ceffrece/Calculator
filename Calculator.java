14da5e549bd84f04457683a1569ab049d4742291
Ceffrece <cheffrece@quinnipiac.edu>
2023-02-08 11:02:04 -0500

    Allows users to add decimals in their numbers by making all types doubles rather than ints

--- Calculator.java
+++ Calculator.java
@@ -5,15 +5,15 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); //Scanner
 
-        System.out.println("Please enter two whole numbers"); //Prompts user two enter two ints
+        System.out.println("Please enter two numbers"); //Prompts user two enter two ints
         System.out.print("Number 1: "); //Number 1
-        int number1 = scan.nextInt(); //Reads in number 1
+        double number1 = scan.nextDouble(); //Reads in number 1
         System.out.print("Number 2: "); //Number 2
-        int number2 = scan.nextInt(); //Readsin number 2
-        int sum = number1 + number2; //Adds together
-        int difference = number1 - number2; //Subtracts the two numbers
-        int multiplication = number1 * number2; //Multiplies the two numbers
-        int division = number1/number2; //Divides the two numbers
+        double number2 = scan.nextDouble(); //Readsin number 2
+        double sum = number1 + number2; //Adds together
+        double difference = number1 - number2; //Subtracts the two numbers
+        double multiplication = number1 * number2; //Multiplies the two numbers
+        double division = number1/number2; //Divides the two numbers
         System.out.println("Would you like to: \n 1: Add \n 2: Subtract \n 3: Multiply \n 4: Divide");
         System.out.println("Choice> ");
         int choice1 = scan.nextInt();
